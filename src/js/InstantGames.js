8b47fe210c2bb46fde73e70f31b95434a457f851
Satoru <jonathantoyoshima@hotmail.com>
2019-02-24 14:01:17 -0300

    Update FB Instant

--- src/js/InstantGames.js
+++ src/js/InstantGames.js
@@ -1,12 +1,29 @@
 window.onload = function(){
-  //FBInstant.initializeAsync().then(function() {
-    //FBInstant.startGameAsync().then(function() {
-      //var contextId = FBInstant.context.getID();
-      //var contextType = FBInstant.context.getType();
-      //var playerName = FBInstant.player.getName();
-      //var playerPic = FBInstant.player.getPhoto();
-      //var playerId = FBInstant.player.getID();
-    //});
+  FBInstant.initializeAsync().then(function() {
+    FBInstant.startGameAsync().then(function() {
+      var playerName = FBInstant.player.getName();
+      var playerPic = FBInstant.player.getPhoto();
+      var playerId = FBInstant.player.getID();
+      var contextId = FBInstant.context.getID();
+      var contextType = FBInstant.context.getType();
+
+      document.querySelector('.playerName').innerHTML = "Name: "+ playerName;
+      document.querySelector('.playerPic').style.backgroundImage = "url("+playerPic+")";
+      document.querySelector('.playerId').innerHTML = "Id: " + playerId;
+      document.querySelector('.contextId').innerHTML = "ContextId: " + contextId;
+      document.querySelector('.contextType').innerHTML = "Context Type: " + contextType;
+
+      // var connectedPlayers = FBInstant.player.getConnectedPlayersAsync()
+      // .then(function(players) {
+      //   console.log(players.map(function(player) {
+      //     return {
+      //       id: player.getID(),
+      //       name: player.getName(),
+      //     }
+      //   }));
+      // });
+
   // Start loading game assets here
-  //});
+  });
+})
 }
